75e03754afd72b693a05166a1de0677e0e56181c
Gustavo Martin <gusma62@gmail.com>
2019-09-19 13:17:42 -0500

    Semilla de usuarios de ejemplo

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+      $user = new User();
+      $user->name = 'User';
+      $user->email = 'user@example.com';
+      $user->password = bcrypt('secret');
+      $user->save();
+
+      $admin = new User();
+      $admin->name = 'Manager Name';
+      $admin->email = 'admin@example.com';
+      $admin->password = bcrypt('secret');
+      $admin->save();
+    }
+}
