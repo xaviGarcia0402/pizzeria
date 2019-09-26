d721303a7d7ec0185c985961df6ca36a911b23e2
Gustavo Martin <gusma62@gmail.com>
2019-09-26 13:29:52 -0500

    Custom checkbox

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -41,9 +41,9 @@
 
               <div class="form-group row">
                 <div class="col-md-6 offset-md-4">
-                  <div class="form-check">
-                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-                    <label class="form-check-label" for="remember">
+                  <div class="form-check custom-checkbox">
+                    <input class="custom-control-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                    <label class="custom-control-label" for="remember">
                       {{ __('Remember Me') }}
                     </label>
                   </div>
