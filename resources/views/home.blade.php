26bf1853953322b8a8f793c7b37efd1ae3dbf996
Gustavo Martin <gusma62@gmail.com>
2019-09-26 14:45:20 -0500

    Reacomodo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,27 +3,21 @@
 @section('content')
 <div class="container">
 
-  <div class="row justify-content-center">
-    <div class="col-md-8">
+  @if (session('status'))
+    <div class="alert alert-success" role="alert">
+      {{ session('status') }}
+    </div>
+  @endif
+
+  <div class="row">
+    <div class="col-md-3">
 
       <div class="card">
-        <div class="card-header">Dashboard</div>
-          <div class="card-body">
-
-            @if (session('status'))
-              <div class="alert alert-success" role="alert">
-                {{ session('status') }}
-              </div>
-            @endif
-
-            Estás autenticado como:
-
-            @if(Auth::user()->hasRole('admin'))
-              administrador
-            @else
-              usuario normal
-            @endif
-
+        <div class="card-header">Mis roles asignados</div>
+          <div class="card-body p-2">
+            @foreach(Auth::user()->roles as $rol)
+              <span class="badge badge-primary"><i class="fa fa-check"></i> {{ $rol->name }}</span>
+            @endforeach
           </div><!-- /.card-body -->
         </div><!-- /.card -->
 
