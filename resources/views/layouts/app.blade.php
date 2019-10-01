9d2691f75f826ec7ed69336fb360af8642906fc3
Gustavo Martin <gusma62@gmail.com>
2019-10-01 14:20:31 -0500

    Botón a home

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,7 +35,11 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <!-- Left Side Of Navbar -->
           <ul class="navbar-nav mr-auto">
-
+            @auth
+              <li class="nav-item">
+                <a class="nav-link" href="{{ route('home') }}">{{ __('Home') }}</a>
+              </li>
+            @endauth
           </ul>
 
           <!-- Right Side Of Navbar -->
