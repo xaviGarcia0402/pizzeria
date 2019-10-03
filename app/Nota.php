7c1a8b059cca48a25e940c3f76e691ade39a0b25
Gustavo Martin <gusma62@gmail.com>
2019-10-03 14:54:23 -0500

    Aplicación la librería de LogsActivity en el modelo de las notas

--- app/Nota.php
+++ app/Nota.php
@@ -3,8 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\Traits\LogsActivity;
 
 class Nota extends Model
 {
-    //
+  use LogsActivity;
+  protected static $logAttributes = ['nombre','descripcion'];
+  protected static $logOnlyDirty = true;
 }
