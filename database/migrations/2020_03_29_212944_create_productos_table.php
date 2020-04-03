f7130ca888ebc302e451a5c080c6a186dc2ab08f
jmusun12 <stdjosemusun@gmail.com>
2020-04-03 17:02:11 -0600

    prueba

---
+++ database/migrations/2020_03_29_212944_create_productos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('producto', 250);
+            $table->string('descripcion', 250);
+            $table->string('precioVenta', 250);
+            $table->string('precioCompra', 250);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('productos');
+    }
+}
